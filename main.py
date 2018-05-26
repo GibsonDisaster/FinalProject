6e803abb00806f4a53ffa7c3364475ff6d4cbf44
Henning Tonko <henninghtonko@gmail.com>
2018-05-26 16:40:54 -0500

    added animations

--- main.py
+++ main.py
@@ -12,14 +12,39 @@ import pygame
     Make good
 '''
 
+def flatten(lst):
+  r = []
+
+  for x in lst:
+    r.append(x[0])
+    r.append(x[1])
+
+  return r
+
 display_width = 800
 display_height = 600
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+pygame.font.init()
+
 # Load images
 intro_logo = pygame.image.load("res/logo.png")
+
+point_images = []
+point_links = ["res/p0.png", "res/p1.png", "res/p2.png", "res/p3.png", "res/p4.png", "res/p5.png"]
+
+for link in point_links:
+  point_images.append(pygame.image.load(link))
+
+test = []
+image_links = ["res/mouth1.png", "res/mouth2.png", "res/mouth3.png", "res/mouth4.png", "res/mouth5.png"]
+
+zipped_images = list(zip(image_links, image_links))
+
+for link in flatten(zipped_images):
+  test.append(pygame.image.load(link))
 
 class Paddle:
 
@@ -62,6 +87,11 @@ def main():
   p2 = Paddle(1180, 0, 50, 300)
   ball = Ball(300, 400)
 
+  counter = 0
+
+  point1 = 0
+  point2 = 0
+
   done = False
   show_logo = True
   play_game = False
@@ -86,8 +116,15 @@ def main():
       # Update
 
       # Collide with screen edges
-      if (ball.x + ball.vx >= 1280 or ball.x + ball.vx <= 0):
-        ball.vx *= (-1)
+      if (ball.x + ball.vx >= 1280):
+        point1 += 1
+        ball.x = 600
+      else:
+        ball.x += ball.vx
+
+      if (ball.x + ball.vx <= 0):
+        point2 += 1
+        ball.x = 600
       else:
         ball.x += ball.vx
 
@@ -118,6 +155,8 @@ def main():
             p1.v = 0
           if event.key == pygame.K_s:
             p1.v = 10
+          if event.key == pygame.K_ESCAPE:
+            done = True
         elif event.type == pygame.KEYUP:
             p1.v = 0
             p2.v = 0
@@ -127,10 +166,20 @@ def main():
 
       # Render
       gameDisplay.fill(black)
+      gameDisplay.blit(test[counter], (100, 100))
+      gameDisplay.blit(point_images[point1], (100, 0))
+      gameDisplay.blit(point_images[point2], (1180, 0))
+
       pygame.draw.rect(gameDisplay, white, (p1.x, p1.y, p1.width, p1.height), 3)
       pygame.draw.rect(gameDisplay, white, (p2.x, p2.y, p2.width, p2.height), 3)
       pygame.draw.circle(gameDisplay, white, (ball.x, ball.y), ball.r, 3)
+      
       pygame.display.flip()
+
+      if counter == len(test) - 1:
+        counter = 0
+      else:
+        counter += 1
 
     pygame.display.update()
     clock.tick(60)
